ac87cfa6112739524c38ede4f3f7084e3b55cd7a
Florian Wuermseer <wuermsee@hm.edu>
2021-08-15 15:16:16 +0200

    Support editing todos and deleting sections

--- src/extension.ts
+++ src/extension.ts
@@ -1,41 +1,80 @@
-import * as vscode from 'vscode';
-import { v4 as uuidv4 } from 'uuid';
-import { window, commands } from 'vscode';
-import { TreeView, TODO, TODOData } from './tree-view';
-import { match, __ } from 'ts-pattern';
+import * as vscode from "vscode";
+import { v4 as uuidv4 } from "uuid";
+import { window, commands } from "vscode";
+import { TreeView, TODO, TODOData } from "./tree-view";
+import { match, __ } from "ts-pattern";
 
 // this method is called when the extension is activated
 export function activate(context: vscode.ExtensionContext) {
+  const treeView: TreeView = new TreeView(context);
+  context.globalState.get("todo-hierarchy-tree") || context.globalState.update("todo-hierarchy-tree", []);
 
-	const treeView: TreeView = new TreeView(context);
-	context.globalState.get('todo-hirachie-tree') || context.globalState.update('todo-hirachie-tree', []);
-
-	// Register todo view
-	vscode.window.registerTreeDataProvider('todo', treeView);
-	// Register commands for checking
-	context.subscriptions.push(commands.registerCommand('todo.check', async (todo: TODO) => todo.id && updateGlobalState(context, treeView, check(todo.id))));
-	context.subscriptions.push(commands.registerCommand('todo.uncheck', async (todo: TODO) => todo.id && updateGlobalState(context, treeView, check(todo.id))));
-	context.subscriptions.push(commands.registerCommand('todo.dir.add', async (todo: TODO) => showInputBox(context, treeView, inputBox, createElem(false), add, todo)));
-	context.subscriptions.push(commands.registerCommand('todo.dir.create-dir', async (todo: TODO) => showInputBox(context, treeView, inputBox, createElem(true), add, todo)));
-	context.subscriptions.push(commands.registerCommand('todo.add', async () => showInputBox(context, treeView, inputBox, createElem(false), addBase)));
-	context.subscriptions.push(commands.registerCommand('todo.create-dir', async () => showInputBox(context, treeView, inputBox, createElem(true), addBase)));
-	context.subscriptions.push(commands.registerCommand('todo.remove-done', async () => updateGlobalState(context, treeView, removeDone)));
+  // Register todo view
+  vscode.window.registerTreeDataProvider("todo", treeView);
+  // Register commands for checking
+  context.subscriptions.push(
+    commands.registerCommand(
+      "todo.check",
+      async (todo: TODO) => todo.id && updateGlobalState(context, treeView, check(todo.id))
+    )
+  );
+  context.subscriptions.push(
+    commands.registerCommand(
+      "todo.edit",
+      async (todo: TODO) => todo.id && showInputBox(context, treeView, inputBox, createElem(false), edit, todo)
+    )
+  );
+  context.subscriptions.push(
+    commands.registerCommand(
+      "todo.uncheck",
+      async (todo: TODO) => todo.id && updateGlobalState(context, treeView, check(todo.id))
+    )
+  );
+  context.subscriptions.push(
+    commands.registerCommand("todo.dir.add", async (todo: TODO) =>
+      showInputBox(context, treeView, inputBox, createElem(false), add, todo)
+    )
+  );
+  context.subscriptions.push(
+    commands.registerCommand("todo.dir.create-dir", async (todo: TODO) =>
+      showInputBox(context, treeView, inputBox, createElem(true), add, todo)
+    )
+  );
+  context.subscriptions.push(
+    commands.registerCommand(
+      "todo.dir.remove",
+      async (todo: TODO) => todo.id && updateGlobalState(context, treeView, remove(todo.id))
+    )
+  );
+  context.subscriptions.push(
+    commands.registerCommand("todo.add", async () =>
+      showInputBox(context, treeView, inputBox, createElem(false), addBase)
+    )
+  );
+  context.subscriptions.push(
+    commands.registerCommand("todo.create-dir", async () =>
+      showInputBox(context, treeView, inputBox, createElem(true), addBase)
+    )
+  );
+  context.subscriptions.push(
+    commands.registerCommand("todo.remove-done", async () => updateGlobalState(context, treeView, removeDone))
+  );
 }
 
 /**
- * Function that modifies the data sotore (vscode nosql db)
+ * Function that modifies the data store (vscode no-sql db)
  * @param context Extension state
- * @param view View to update after modifiying
- * @param modFunction 
+ * @param view View to update after modifying
+ * @param modFunction
  */
 async function updateGlobalState(context: vscode.ExtensionContext, view: TreeView, modFunction: CallableFunction) {
-	const todoData: TODOData[] | undefined = context.globalState.get('todo-hirachie-tree');
-	if (!todoData) {
-		throw new Error('You checked a todo that doesnt exist');
-	}
-	const newState = modFunction(todoData);
-	context.globalState.update('todo-hirachie-tree', newState);
-	view.refresh();
+  const todoData: TODOData[] | undefined = context.globalState.get("todo-hierarchy-tree");
+  if (!todoData) {
+    throw new Error("You checked a todo that doesn't exist");
+  }
+  const newState = modFunction(todoData);
+  context.globalState.update("todo-hierarchy-tree", newState);
+  view.refresh();
 }
 
 /**
@@ -49,67 +88,133 @@ async function updateGlobalState(context: vscode.ExtensionContext, view: TreeVie
  * @returns void
  */
 const showInputBox = async (
-	context: vscode.ExtensionContext,
-	treeView: TreeView,
-	uiElem: () => Promise<string|undefined>,
-	createElem: (name: string) => TODOData,
-	modFunction: CallableFunction,
-	param?: TODO
-) => uiElem().then((inputText: string|undefined) => inputText? updateGlobalState(
-		context,
-		treeView,
-		param ?
-			modFunction (createElem(inputText)) (param.id):
-			modFunction (createElem(inputText))
-): console.error('Invalid transaction'));
+  context: vscode.ExtensionContext,
+  treeView: TreeView,
+  uiElem: () => Promise<string | undefined>,
+  createElem: (name: string) => TODOData,
+  modFunction: CallableFunction,
+  param?: TODO
+) =>
+  uiElem().then((inputText: string | undefined) =>
+    inputText
+      ? updateGlobalState(
+          context,
+          treeView,
+          param ? modFunction(param.id)(createElem(inputText)) : modFunction(createElem(inputText))
+        )
+      : console.error("Invalid transaction")
+  );
 
-type InputBox = () => Promise<string|undefined>;
-const inputBox: InputBox = async () => await window.showInputBox({
-	placeHolder: 'For example: Install GNOME!',
-});
+type Remove = (id: string) => (dataStore: TODOData[]) => TODOData[];
+const remove: Remove = (id) => (dataStore) =>
+  dataStore
+    .filter((todoData: TODOData) =>
+      match(todoData)
+        .with(undefined, () => true)
+        .with({ label: __.string, uuid: __.string, checked: __.boolean }, () => true)
+        .with({ name: __.string, id: id, content: __ }, () => false)
+        .with({ name: __.string, id: __.string, content: __ }, () => true)
+        .exhaustive()
+    )
+    .map((elem: TODOData) =>
+      match(elem)
+        .with(undefined, () => elem)
+        .with({ label: __.string, uuid: __.string, checked: __.boolean }, () => elem)
+        .with({ name: __.string, id: __.string, content: __ }, (subDir) => ({
+          name: subDir.name,
+          id: subDir.id,
+          content: remove(id)(subDir.content),
+        }))
+        .exhaustive()
+    );
+
+type InputBox = () => Promise<string | undefined>;
+const inputBox: InputBox = async () =>
+  await window.showInputBox({
+    placeHolder: "For example: Install GNOME!",
+  });
 
 type CreateElem = (isDirectory: boolean) => (name: string) => TODOData;
-const createElem: CreateElem = (isDirectory) => isDirectory ?
-	(name: string) => ({ name: name, id: uuidv4(), content: [] }):
-	(name: string) => ({ label: name, uuid: uuidv4(), checked: false });
+const createElem: CreateElem = (isDirectory) =>
+  isDirectory
+    ? (name: string) => ({ name: name, id: uuidv4(), content: [] })
+    : (name: string) => ({ label: name, uuid: uuidv4(), checked: false });
 
-// 
 type Check = (id: string) => (dataStore: TODOData[]) => TODOData[];
-const check: Check = (id) => (dataStore) => dataStore
-	.map((todoData: TODOData) => match(todoData)
-		.with(undefined, () => todoData)
-		.with({ label: __.string, uuid: id, checked: __.boolean }, (elem) => (
-			{ label: elem.label, uuid: elem.uuid, checked: !elem.checked }))
-		.with({ label: __.string, uuid: __.string, checked: __.boolean }, () => todoData)
-		.with({ name: __.string, id: __.string, content: __ }, (subDir) => (
-			{ name: subDir.name, id: subDir.id, content: check(id)(subDir.content) }))
-		.exhaustive());
+const check: Check = (id) => (dataStore) =>
+  dataStore.map((todoData: TODOData) =>
+    match(todoData)
+      .with(undefined, () => todoData)
+      .with({ label: __.string, uuid: id, checked: __.boolean }, (elem) => ({
+        label: elem.label,
+        uuid: elem.uuid,
+        checked: !elem.checked,
+      }))
+      .with({ label: __.string, uuid: __.string, checked: __.boolean }, () => todoData)
+      .with({ name: __.string, id: __.string, content: __ }, (subDir) => ({
+        name: subDir.name,
+        id: subDir.id,
+        content: check(id)(subDir.content),
+      }))
+      .exhaustive()
+  );
 
-type Add = (elem: TODOData) => (dirId: string) => (dataStore: TODOData[]) => TODOData[];
-const add: Add = (elem) => (id) => (dataStore) => dataStore
-	.map((todoData: TODOData) => match(todoData)
-		.with(undefined, () => todoData)
-		.with({ label: __.string, uuid: __.string, checked: __.boolean }, () => todoData)
-		.with({ name: __.string, id: id, content: __ }, (subDir) => (
-			{ name: subDir.name, id: subDir.id, content: subDir.content.concat(elem) }))
-		.with({ name: __.string, id: __.string, content: __ }, (subDir) => (
-			{ name: subDir.name, id: subDir.id, content: add(elem)(id)(subDir.content) }))
-		.exhaustive());
+type Add = (dirId: string) => (elem: TODOData) => (dataStore: TODOData[]) => TODOData[];
+const add: Add = (id) => (elem) => (dataStore) =>
+  dataStore.map((todoData: TODOData) =>
+    match(todoData)
+      .with(undefined, () => todoData)
+      .with({ label: __.string, uuid: __.string, checked: __.boolean }, () => todoData)
+      .with({ name: __.string, id: id, content: __ }, (subDir) => ({
+        name: subDir.name,
+        id: subDir.id,
+        content: subDir.content.concat(elem),
+      }))
+      .with({ name: __.string, id: __.string, content: __ }, (subDir) => ({
+        name: subDir.name,
+        id: subDir.id,
+        content: add(id)(elem)(subDir.content),
+      }))
+      .exhaustive()
+  );
 
 type AddBase = (elem: TODOData) => (dataStore: TODOData[]) => TODOData[];
 const addBase: AddBase = (elem) => (dataStore) => dataStore.concat(elem);
 
 type RemoveDone = (dataStore: TODOData[]) => TODOData[];
-const removeDone: RemoveDone = (dataStore) => dataStore
-	.filter((todoData: TODOData) => match(todoData)
-		.with(undefined, () => true)
-		.with({ label: __.string, uuid: __.string, checked: true }, () => false)
-		.with({ label: __.string, uuid: __.string, checked: __.boolean }, () => true)
-		.with({ name: __.string, id: __.string, content: __ }, () => true)
-		.exhaustive())
-	.map((elem: TODOData) => match(elem)
-		.with(undefined, () => elem)
-		.with({ label: __.string, uuid: __.string, checked: __.boolean }, () => elem)
-		.with({ name: __.string, id: __.string, content: __ }, (subDir) => (
-			{ name: subDir.name, id: subDir.id, content: removeDone(subDir.content) }))
-		.exhaustive());
+const removeDone: RemoveDone = (dataStore) =>
+  dataStore
+    .filter((todoData: TODOData) =>
+      match(todoData)
+        .with(undefined, () => true)
+        .with({ label: __.string, uuid: __.string, checked: true }, () => false)
+        .with({ label: __.string, uuid: __.string, checked: __.boolean }, () => true)
+        .with({ name: __.string, id: __.string, content: __ }, () => true)
+        .exhaustive()
+    )
+    .map((elem: TODOData) =>
+      match(elem)
+        .with(undefined, () => elem)
+        .with({ label: __.string, uuid: __.string, checked: __.boolean }, () => elem)
+        .with({ name: __.string, id: __.string, content: __ }, (subDir) => ({
+          name: subDir.name,
+          id: subDir.id,
+          content: removeDone(subDir.content),
+        }))
+        .exhaustive()
+    );
+
+type Edit = (id: string) => (elem: TODOData) => (dataStore: TODOData[]) => TODOData[];
+const edit: Edit = (id) => (elem) => (dataStore) =>
+  dataStore.map((todoData: TODOData) =>
+    match(todoData)
+      .with(undefined, () => todoData)
+      .with({ label: __.string, uuid: id, checked: __.boolean }, () => elem)
+      .with({ label: __.string, uuid: __.string, checked: __.boolean }, () => todoData)
+      .with({ name: __.string, id: __.string, content: __ }, (subDir) => ({
+        name: subDir.name,
+        id: subDir.id,
+        content: edit(id)(elem)(subDir.content),
+      }))
+      .exhaustive()
+  );
--- src/tree-view.ts
+++ src/tree-view.ts
@@ -1,10 +1,10 @@
-import { match, __ } from 'ts-pattern';
-import * as vscode from 'vscode';
+import { match, __ } from "ts-pattern";
+import * as vscode from "vscode";
 
 export type TODOData =
-	| undefined
-	| {label: string, uuid: string, checked: boolean}
-	| {name: string, id: string, content: TODOData[]};
+  | undefined
+  | { label: string; uuid: string; checked: boolean }
+  | { name: string; id: string; content: TODOData[] };
 
 export class TODO extends vscode.TreeItem {
   constructor(
@@ -19,20 +19,24 @@ export class TODO extends vscode.TreeItem {
     this.id = this.uuid;
   }
 
-  contextValue = this.collapsibleState !== vscode.TreeItemCollapsibleState.None?
-    'todo-dir': this.checked?
-    'checked': 'unchecked';
+  contextValue =
+    this.collapsibleState !== vscode.TreeItemCollapsibleState.None
+      ? "todo-dir"
+      : this.checked
+      ? "checked"
+      : "unchecked";
 }
 
-export class TreeView implements vscode.TreeDataProvider<TODO>{
+export class TreeView implements vscode.TreeDataProvider<TODO> {
+  private _onDidChangeTreeData: vscode.EventEmitter<TODO | undefined | void> = new vscode.EventEmitter<
+    TODO | undefined | void
+  >();
+  readonly onDidChangeTreeData: vscode.Event<TODO | undefined | void> = this._onDidChangeTreeData.event;
 
-  private _onDidChangeTreeData: vscode.EventEmitter<TODO | undefined | void> = new vscode.EventEmitter<TODO | undefined | void>();
-	readonly onDidChangeTreeData: vscode.Event<TODO | undefined | void> = this._onDidChangeTreeData.event;
-
-  constructor(private context: vscode.ExtensionContext) { }
+  constructor(private context: vscode.ExtensionContext) {}
 
   /**
-   * This is an overwrite of TreeDataProvider in order to display cursom items
+   * This is an overwrite of TreeDataProvider in order to display custom items
    * @param element Currently selected treeView implementation
    * @returns Selected item
    */
@@ -44,26 +48,28 @@ export class TreeView implements vscode.TreeDataProvider<TODO>{
    * This is an overwrite of TreeDataProvider that is called on vscode update events
    */
   refresh(): void {
-		this._onDidChangeTreeData.fire();
-	}
+    this._onDidChangeTreeData.fire();
+  }
 
   // This is an overwrite of TreeDataProvider that is triggered whenever a folder is getting expanded
   getChildren(todo?: TODO): vscode.ProviderResult<TODO[]> {
-    // Read from vscode nosql db
-    const todoData: TODOData[]|undefined = this.context.globalState.get('todo-hirachie-tree');
+    // Read from vscode nos-ql db
+    const todoData: TODOData[] | undefined = this.context.globalState.get("todo-hierarchy-tree");
     // If reading was unsuccessful return no further todos
     if (!todoData) {
       return [];
     }
     // Todo exists whenever the current node is not the root node
     if (todo) {
-      const parent: TODOData = todoData.map((entry: TODOData) => mapToParent(entry)(todo.id)).filter((entry) => entry)[0];
-      const children: TODOData[]|undefined = match(parent)
-          .with(undefined, () => undefined)
-          .with({label: __.string, uuid: __.string, checked: __.boolean}, () => [])
-          .with({name: __.string, id: __.string, content: __}, (elem) => elem.content)
-          .exhaustive();
-      return children? mapDataToTreeItem(children): [];
+      const parent: TODOData = todoData
+        .map((entry: TODOData) => mapToParent(entry)(todo.id))
+        .filter((entry) => entry)[0];
+      const children: TODOData[] | undefined = match(parent)
+        .with(undefined, () => undefined)
+        .with({ label: __.string, uuid: __.string, checked: __.boolean }, () => [])
+        .with({ name: __.string, id: __.string, content: __ }, (elem) => elem.content)
+        .exhaustive();
+      return children ? mapDataToTreeItem(children) : [];
     }
     // Set new todos
     return mapDataToTreeItem(todoData);
@@ -72,33 +78,45 @@ export class TreeView implements vscode.TreeDataProvider<TODO>{
 
 type MapDataToTreeItem = (todoData: TODOData[]) => TODO[];
 const mapDataToTreeItem: MapDataToTreeItem = (todoData: TODOData[]) =>
-todoData.map((entry: TODOData) =>
-  match(entry)
-    .with(undefined, () => new TODO('...', '', false, vscode.TreeItemCollapsibleState.None))
-    .with({label: __.string, uuid: __.string, checked: true}, (elem) =>
-      new TODO(strikeThrough(elem.label), elem.uuid, elem.checked, vscode.TreeItemCollapsibleState.None))
-    .with({label: __.string, uuid: __.string, checked: __.boolean}, (elem) =>
-      new TODO(elem.label, elem.uuid, elem.checked, vscode.TreeItemCollapsibleState.None))
-    .with({name: __.string, id: __.string, content: __}, (elem) =>
-      new TODO(elem.name, elem.id, false, vscode.TreeItemCollapsibleState.Collapsed))
-    .exhaustive());
+  todoData.map((entry: TODOData) =>
+    match(entry)
+      .with(undefined, () => new TODO("...", "", false, vscode.TreeItemCollapsibleState.None))
+      .with(
+        { label: __.string, uuid: __.string, checked: true },
+        (elem) => new TODO(strikeThrough(elem.label), elem.uuid, elem.checked, vscode.TreeItemCollapsibleState.None)
+      )
+      .with(
+        { label: __.string, uuid: __.string, checked: __.boolean },
+        (elem) => new TODO(elem.label, elem.uuid, elem.checked, vscode.TreeItemCollapsibleState.None)
+      )
+      .with(
+        { name: __.string, id: __.string, content: __ },
+        (elem) => new TODO(elem.name, elem.id, false, vscode.TreeItemCollapsibleState.Collapsed)
+      )
+      .exhaustive()
+  );
 
 type MapToParent = (entry: TODOData) => (id: string | undefined) => TODOData;
 const mapToParent: MapToParent = (entry: TODOData) => (id: string | undefined) =>
-  id? match(entry)
-    .with(undefined, () => undefined)
-    .with({label: __.string, uuid: __.string, checked: __.boolean}, () => undefined)
-    .with({name: __.string, id: __.string, content: []}, () => undefined)
-    .with({name: __.string, id: __.string, content: __}, (elem) => elem.id === id? elem: elem.content
-        .map((elem: TODOData) => mapToParent(elem)(id))
-        .reduce((prev: TODOData, current: TODOData) => prev || current))
-    .exhaustive():
-    // False in case no id was defined
-  undefined;
+  id
+    ? match(entry)
+        .with(undefined, () => undefined)
+        .with({ label: __.string, uuid: __.string, checked: __.boolean }, () => undefined)
+        .with({ name: __.string, id: __.string, content: [] }, () => undefined)
+        .with({ name: __.string, id: __.string, content: __ }, (elem) =>
+          elem.id === id
+            ? elem
+            : elem.content
+                .map((elem: TODOData) => mapToParent(elem)(id))
+                .reduce((prev: TODOData, current: TODOData) => prev || current)
+        )
+        .exhaustive()
+    : // False in case no id was defined
+      undefined;
 
 type StrikeThrough = (text: string) => string;
 const strikeThrough: StrikeThrough = (text: string) =>
   text
-    .split('')
-    .map(char => char + '\u0336')
-    .join('');
+    .split("")
+    .map((char) => char + "\u0336")
+    .join("");
